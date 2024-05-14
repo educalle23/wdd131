2cab445dc0d55a362d5087a768d3f671c7968ad0
educalle23 <edu.lds.23@gmail.com>
2024-05-13 23:30:11 -0500

    done

--- week02/scripts/temples.js
+++ week02/scripts/temples.js
@@ -8,3 +8,13 @@ let lastModifiedDate = document.lastModified;
 
 fullYear.innerHTML = `${currentYear} Eduardo Calle | Trujillo - Peru`;
 lastModified.innerHTML = `Last Modification: ${lastModifiedDate}`;
+
+
+const button = document.querySelector('#nav-menu');
+const menu = document.querySelector('.navigator');
+
+button.addEventListener('click', () => {
+    console.log('Heme aqui')
+    menu.classList.toggle('open');
+    button.classList.toggle('open');
+});
