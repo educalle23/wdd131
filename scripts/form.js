4cb4655f3e47986c2bfd8472ed4fda581dd4ee79
educalle23 <edu.lds.23@gmail.com>
2024-06-05 23:10:46 -0500

    doneee

--- scripts/form.js
+++ scripts/form.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
     const products = [
-        { id: 'fc-1888', name: 'flux capacitor', avg_rating: 4.5 },
-        { id: 'fc-2050', name: 'power laces', avg_rating: 4.7 },
-        { id: 'fs-1987', name: 'time circuits', avg_rating: 3.5 },
-        { id: 'ac-2000', name: 'low voltage reactor', avg_rating: 3.9 },
-        { id: 'jj-1969', name: 'warp equalizer', avg_rating: 5.0 }
+        { id: 'fc-1888', name: 'Flux capacitor', avg_rating: 4.5 },
+        { id: 'fc-2050', name: 'Power laces', avg_rating: 4.7 },
+        { id: 'fs-1987', name: 'Time circuits', avg_rating: 3.5 },
+        { id: 'ac-2000', name: 'Low voltage reactor', avg_rating: 3.9 },
+        { id: 'jj-1969', name: 'Warp equalizer', avg_rating: 5.0 }
     ];
 
     const productSelect = document.getElementById('product-name');
